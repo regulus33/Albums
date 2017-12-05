9aaa61a2f1a55e021e70f38e99c919e5340eec60
Regulus33 <zacharyrowden89@gmail.com>
2017-12-05 15:48:13 -0500

    Get api data in albumlist.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,7 +3,18 @@ import {View, Text} from 'react-native';
 
 class AlbumList extends Component {
 
+  state = { albums: [] };
+
+  componentWillMount() {
+     fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then(data => this.setState({ albums: data }));
+  }
+
   render(){
+
+    console.log(this.state)
+
     return(
       <View>
         <Text>AlbumList!!!</Text>
