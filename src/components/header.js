618e4cc7a714fb9ae6211c69b8300775b429d414
Regulus33 <zacharyrowden89@gmail.com>
2017-12-05 14:27:05 -0500

    Explore flexbox and shadow styles.

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,24 @@ const Header = () => {
     );
 }
 
-const styles = {textStyle: {fontSize:20}, viewStyle:{backgroundColor: '#F8F8F8'}}
+const styles = {
+  textStyle: {
+    fontSize:20
+  },
+  viewStyle:{
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+
+}
 
 
 export default Header
