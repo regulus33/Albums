1c87c145f8eda4762d911e88e75bee4688e9b7e6
Regulus33 <zacharyrowden89@gmail.com>
2017-12-05 13:55:59 -0500

    Add component directories and header.

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+  //text tag is from react native
+  return <Text>Albums</Text>
+}
+
+export default Header
