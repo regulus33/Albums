3ab3ae0f45d626aac875dbe1d8c8c6af1c534f9a
Regulus33 <zacharyrowden89@gmail.com>
2017-12-05 14:14:13 -0500

    Add some styles.

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
+  const {textStyle, viewStyle} = styles
   //text tag is from react native
-  return <Text>Albums</Text>
+  return(
+    <View style = { styles.viewStyle }>
+      <Text style = { textStyle }>Albums</Text>
+    </View>
+    );
 }
+
+const styles = {textStyle: {fontSize:20}, viewStyle:{backgroundColor: '#F8F8F8'}}
+
 
 export default Header
