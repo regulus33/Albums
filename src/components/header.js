4440b4341fad2528a2b310c43ad059c8938e53a3
Regulus33 <zacharyrowden89@gmail.com>
2017-12-05 14:42:48 -0500

    Add dynamic text through props.

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
   const {textStyle, viewStyle} = styles
   //text tag is from react native
   return(
     <View style = { styles.viewStyle }>
-      <Text style = { textStyle }>Albums</Text>
+      <Text style = { textStyle }>{props.headerText}</Text>
     </View>
     );
 }
