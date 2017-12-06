c5423a42d82dcb0600dde2ab5a5208bbd5ae830f
Regulus33 <zacharyrowden89@gmail.com>
2017-12-06 12:31:48 -0500

    Add card component.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
 
@@ -17,7 +18,7 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map( album => <Text>{album.title}</Text> )
+    return this.state.albums.map( album => <AlbumDetail key={album.title} album={album}/>)
   }
 
   render(){
@@ -26,7 +27,7 @@ class AlbumList extends Component {
 
     return(
       <View>
-        <Text>{this.renderAlbums()}</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
